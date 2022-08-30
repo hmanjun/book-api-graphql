c6e8fe7027275ccdb3853491a52e2f0a547f0b35
hmanjun <harshith.hm03@gmail.com>
2022-08-29 18:36:56 -0700

    Add typeDef for createUser/login mutation

---
+++ server/schemas/typeDef.js
@@ -0,0 +1,34 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        profile: Profile
+    }
+
+    type Query {
+        getSingleUser(_id: ID!): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!)
+        login(email: String!, password: String!): Auth
+    }
+`
